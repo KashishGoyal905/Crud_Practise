25eb2def10fac138aec9495676abc8d5c197a35f
Kashish Goyal <Kashishgoyal961@gmail.com>
2021-07-25 11:02:53 +0530

    show page

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path = require('path');
 
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const { createSecretKey } = require('crypto');
 
 mongoose.connect('mongodb://localhost:27017/Crud', {
     useNewUrlParser: true,
@@ -15,14 +16,33 @@ mongoose.connect('mongodb://localhost:27017/Crud', {
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log(" Crud Database connected");
+    console.log("Crud Database connected");
 });
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
+app.get('/campgrounds', async (req, res) => {
+    // const camp = new Campground({ title: "kings place", price: 40, location: "west bihar,UP,india" });
+    // await camp.save();
+    const campgrounds = await Campground.find({});
+    res.render('home', { campgrounds });
+});
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+});
+
+app.post('/campgrounds',async (req, res)=>{
+    // const campground = new Campground(req.body.campground);
+    // await campground.save();
+    // res.redirect(`/campgrounds/${campground._id}`);
+});
+
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', { campground });
 });
 
 app.listen(3000, (req, res) => {
